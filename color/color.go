e59c99d20dc6c17ddaa4f9455449f53a5a47cbbc
Nicolas <nicolashereigo@gmail.com>
2022-01-23 16:53:46 +0300

    Added a yellow color coloring function

--- color/color.go
+++ color/color.go
@@ -3,6 +3,7 @@ package color
 var (
 	red                 = "\033[31m"
 	green               = "\033[32m"
+	yellow              = "\033[33m"
 	endColoringSequence = "\033[0m"
 )
 
@@ -12,4 +13,8 @@ func TextToGreen(text string) string {
 
 func TextToRed(text string) string {
 	return red + text + endColoringSequence
+}
+
+func TextToYellow(text string) string {
+	return yellow + text + endColoringSequence
 }
